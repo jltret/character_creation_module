f813e7a2a7d71b80488f8b80a0205ef34a024a77
Стас Басов <stas.basov@whatever.com>
2023-01-29 11:32:23 +0300

    add if __name__

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Character():
 
     BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
     RANGE_VALUE_ATTACK = (1, 3)
-    RANGE_VALUE_DEFENCE = (1, 3)
+    RANGE_VALUE_DEFENCE = (1, 5)
     # Вот они — две новые константы.
     SPECIAL_BUFF = 15
     SPECIAL_SKILL = 'Удача'
@@ -106,3 +106,15 @@ class Healer(Character):
 warrior = Warrior('Кодослав')
 print(warrior)
 print(warrior.attack())
+
+if __name__ == '__main__':
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = Character.choice_char_class()
+    print(Character.start_training(char_name, char_class)) 
