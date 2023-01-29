c6b55fa6b57ebef4f10bf36c42e24ba079a4422f
Стас Басов <stas.basov@whatever.com>
2023-01-29 11:27:37 +0300

    from moduls to classes with moduls

--- Character in classes.py
+++ Character in classes.py
@@ -2,11 +2,6 @@
 
 from random import randint
 
-# Новый импорт.
-# Из модуля start_game_banner, который расположен в папке graphic_arts,
-# импортируем функцию run_screensaver().
-from graphic_arts.start_game_banner import run_screensaver
-
 
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
